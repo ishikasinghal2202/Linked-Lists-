99f1075daa47f256857950c2ce006c3e5fdcf043
Ishika <singhalishika436@gmail.com>
2025-09-26 08:33:17 +0530

    Create Single Linked List using Class.cpp

---
+++ Single Linked List using Class.cpp
@@ -0,0 +1,27 @@
+//Ishika Singhal
+//24070123048
+//CODE:
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node*next;
+    
+    Node(int data){
+        val=data;
+        next=NULL;
+    }
+};
+
+int main() {
+    Node*n=new Node(20);
+    cout<<n->val<<" "<<n->next<<endl;
+
+    return 0;
+}
+
+//OUTPUT:
+//20 0
